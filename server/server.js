98211d1e14d427621ef5c5446186164679552c39
Alokkumar2228 <141926450+Alokkumar2228@users.noreply.github.com>
2025-09-01 22:34:29 +0530

    Complete Folder Structure Added

--- server/server.js
+++ server/server.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-
-
-
-
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors({
